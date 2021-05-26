d8f7ea5d690d3e976c4ffabeafd1ecdce22431b3
wtsbee <beechan920@yahoo.co.jp>
2021-05-27 00:15:31 +0900

    21. URLパラメータを扱う

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Page2 = () => {
   return (
     <div>
       <h1>Page2ページです</h1>
+      <Link to="/page2/999">URL UrlParameter</Link>
     </div>
   );
 };
