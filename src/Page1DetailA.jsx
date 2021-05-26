954e9800e24b3fd19e7887eadef35700c7eb1e0f
wtsbee <beechan920@yahoo.co.jp>
2021-05-27 00:46:49 +0900

    23. stateを渡すページ遷移

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 export const Page1DetailA = () => {
+  const { state } = useLocation();
+  console.log(state);
+
   return (
     <div>
       <h1>Page1DetailAページです</h1>
