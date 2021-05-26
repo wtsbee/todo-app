4bc698385dc1c266c9295fbf21da1d0f82ad77d8
wtsbee <beechan920@yahoo.co.jp>
2021-05-27 00:31:22 +0900

    22. クエリパラメータを扱う

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -6,6 +6,8 @@ export const Page2 = () => {
     <div>
       <h1>Page2ページです</h1>
       <Link to="/page2/999">URL UrlParameter</Link>
+      <br />
+      <Link to="/page2/999?name=hogehoge">Query UrlParameter</Link>
     </div>
   );
 };
