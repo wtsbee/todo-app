8a1077acb2a14b9ce58f074d5c6dd3dc9b25e042
wtsbee <beechan920@yahoo.co.jp>
2021-05-26 23:25:37 +0900

    19. ネストされたページ遷移

---
+++ src/Page1DetailA.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Page1DetailA = () => {
+  return (
+    <div>
+      <h1>Page1DetailAページです</h1>
+    </div>
+  );
+};
---
+++ src/Page1DetailB.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Page1DetailB = () => {
+  return (
+    <div>
+      <h1>Page1DetailBページです</h1>
+    </div>
+  );
+};
